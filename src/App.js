e67f8799afd6b8fd2816d0d1db1a9c3f54cead35
Ilyass <ilyass.haddoud@gmail.com>
2023-03-15 12:14:26 +0100

    final version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [input, setInput] = useState("");
+  let renderTime = useRef(0);
+  let timerId = useRef(null);
+
+  const handleInput = (e) => {
+    renderTime.current++;
+    setInput(e.target.value);
+  };
+
+  const start = () => {
+    timerId.current = setInterval(() => {
+      setTimer((x) => x + 1);
+      renderTime.current++;
+    }, 1000);
+  };
+  const stop = () => {
+    clearInterval(timerId.current);
+  };
+
+  const reset = () => {
+    stop();
+    if (timer) {
+      setTimer(0);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="timer">
+        <h1>Timer</h1>
+        <input type="text" onChange={(e) => handleInput(e)} />
+        <div className="number">Seconds : {timer}</div>
+        <div className="btn">
+          <button onClick={start}>Start</button>
+          <button onClick={stop}>Stop</button>
+          <button onClick={reset}>Reset</button>
+        </div>
+        <div className="inputValue">{input}</div>
+        <div className="renderTime">Render count : {renderTime.current}</div>
+      </div>
     </div>
   );
 }
